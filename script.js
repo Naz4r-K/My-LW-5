d2c4aa73f92a2ba3bfea866357c6736aa4bf725d
Nazar <nazarij2101@gmail.com>
2026-02-24 20:06:03 +0200

    Nazar

---
+++ script.js
@@ -0,0 +1,11 @@
+// Main application entry point
+console.log('Hello, World!');
+
+// Function example
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+// Variable examples
+const message = greet('User');
+console.log(message);
